3064bb42fbb69342a4f6e33661ec32c00e12a2a6
khushal <khushal.kedawat@gmail.com>
2025-08-31 13:17:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import routes from "./routes/profile.js";
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
+
+app.use("/api/profiles", routes);
+// Health check
+app.get("/health", (req, res) => res.json({ status: "Everything okay" }));
+
+// DB + Server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch(err => {
+    console.error(" Failed to connect to MongoDB");
+    console.error(err.message);
+  });
+
